158067deafe28569ab02fb107154c83d9eb719c1
Yexi <46566057+tangyuanpro@users.noreply.github.com>
2022-06-28 22:40:09 +0800

    Create config.js

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,17 @@
+export default {
+  // These are app level configs.
+  lang: 'zh-CN',
+  title: 'BimiLinux',
+  description: '一个全新的linux系统',
+  lastUpdated: true,
+  themeConfig: {
+    nav: [
+      { text: 'Guide', link: '/guide' },
+      { text: 'Configs', link: '/configs' },
+      { text: 'Changelog', link: 'https://github.com/...' }
+    ],
+    socialLinks: [
+      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+    ],
+  },
+}
