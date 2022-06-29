4b5a7e0a00f2b959933c8e799cce3eb7b39b5f24
Yexi <46566057+tangyuanpro@users.noreply.github.com>
2022-06-29 17:57:42 +0800

    Create Layout.vue

---
+++ docs/.vitepress/theme/Layout.vue
@@ -0,0 +1,12 @@
+<script setup>
+import DefaultTheme from 'vitepress/theme'
+import { onMounted } from 'vue'
+const { Layout } = DefaultTheme
+onMounted(() => {
+})
+</script>
+
+<template>
+  <Layout />
+  <Artalk />
+</template>
